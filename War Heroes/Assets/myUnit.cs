48f808b8762ee57f8be5c5145e59ac300c7b1615
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2019-02-20 14:55:04 +0500

    unit following mouse

---
+++ War Heroes/Assets/myUnit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myUnit : MonoBehaviour
+{
+
+    public int speed = 100;
+    private Vector3 target;
+
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Input.mousePosition;
+            //target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
